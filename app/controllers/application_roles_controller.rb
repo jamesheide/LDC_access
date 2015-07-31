53e2161965384abd0cd8f00c11262fa8a48f5a86
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2015-07-31 10:47:54 -0400

    select 2 box now dynamically generated with ajax

--- app/controllers/application_roles_controller.rb
+++ app/controllers/application_roles_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationRolesController < ApplicationController
   def index
     @application_roles = ApplicationRole.all
-    @results = { results: [] }
-    @application_roles.each{ |ar| @results[:results] << { id: ar.id, text: "#{ar.application} #{ar.role}" } }
-    render json: @results
+    render json: @application_roles.map{ |ar| { id: ar.id, text: "#{ar.application} #{ar.role}" } }
   end
 end
