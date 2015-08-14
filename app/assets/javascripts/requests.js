a42e183fc2377bddd73be1607043ddcac67da961
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2015-08-14 19:08:11 +0000

    netid is persisting accross pages with persistjs

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+  // global object
+  store = new Persist.Store('My Application');
+  // load persistent store after the DOM has loaded
+  $('#new-request').click( function() {
+    if($.trim($('#netid-input').val()) != '') {    
+      store.set('netid', $('#netid-input').val());
+      alert(store.get('netid'))
+    }
+    else {
+      alert('hey, you need to select a net id before you can make a request');
+      return false;
+    }
+  })
+  $('#tellall').click( function() {
+    alert(store.get('netid'))
+  });
   $.getJSON('/application_roles/index.json', function(data) {
     $.each(data, function(index) {
       $('.application-role-input').append(
