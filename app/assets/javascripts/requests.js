a331bb987650896d15a3ab1db2f82d256ad454fe
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2015-08-14 18:17:49 +0000

    apps working with select2

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $.getJSON('/application_roles/index.json', function(data) {
     $.each(data, function(index) {
-      $('.application_role_input').append(
+      $('.application-role-input').append(
         '<option value="' + data[index].id + '">' + data[index].text + '</option>'
       )
     });
   });
-  $('.application_role_input').select2();
+  $('.application-role-input').select2();
 });
